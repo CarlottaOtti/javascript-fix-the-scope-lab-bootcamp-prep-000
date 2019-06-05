4ca7d18198a6e0eb467a8d3f06175a8d6fe1e469
Carlotta Junin <carlotta.junin@gmail.com>
2019-06-05 16:19:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,13 +14,11 @@ function yourAnimal() {
   return animal
 }
 
-
+var n = 5
 
 function add2(n) {
-  return n + two
-
-  // Feel free to move things around!
-  const two = 2
+const two = 2
+return n + 2
 }
 
 var funkyFunction = function() {
