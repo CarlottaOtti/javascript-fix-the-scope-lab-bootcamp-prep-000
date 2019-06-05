6db33ca46fd36d0790920bf15435c9c0a4aa7953
Carlotta Junin <carlotta.junin@gmail.com>
2019-06-05 16:00:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-var animal = 'dog'
+
 
 function myAnimal() {
-  return animal
-}
+  animal = "dog"
+  return animal;
+
 
 function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
   return animal
+}
 }
 
 function add2(n) {
